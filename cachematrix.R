04097a594910cac30e9e1c573f9718209c1da5e8
Vukinator <35816915+Vukinator@users.noreply.github.com>
2023-07-06 17:24:01 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ i = NULL
+  set = function(y) {
+    x <<- y
+    i <<- NULL
+  } # setting the space for the matrix
+  get = function() x # for recieving the matrix
+  setinverse = function(inverse) i <<- inverse # for setting the inverse
+  getinverse = function() i ### for getting the inverse
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+    i = x$getinverse()
+  if (!is.null(i)) { # condition
+    return(i)
+  }
+  data = x$get()
+  i = solve(data, ...)
+  x$setinverse(i)
+  i
         ## Return a matrix that is the inverse of 'x'
 }
+
+### when testing the programme, the matrix of yours always has to be sqauared
+
+# TEST
+
+# a= matrix(10:13, 2, 2)
+# A= makeCacheMatrix(a)
+# cacheSolve(A)
